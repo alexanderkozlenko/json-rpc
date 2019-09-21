afe555f854a2d7817e2e4aaae5b99c4439628db8
Alexander Kozlenko <alexanderkozlenko@users.noreply.github.com>
2019-09-21 19:35:01 +0300

    Refactor hash code implementation (#57)

---
+++ src/Anemonis.JsonRpc/HashCode.cs
@@ -0,0 +1,10 @@
+﻿// © Alexander Kozlenko. Licensed under the MIT License.
+
+namespace Anemonis.JsonRpc
+{
+    internal static class HashCode
+    {
+        public const int FNV_PRIME_32 = 0x01000193;
+        public const int FNV_OFFSET_BASIS_32 = unchecked((int)0x811C9DC5);
+    }
+}
